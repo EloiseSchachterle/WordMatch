ca4b234c211ba286d93444ef0c94e9a77464dc97
EloiseSchachterle <eloises3@nycstudents.net>
2025-10-14 15:24:50 -0400

    WordMatch

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+WordMatch w = new WordMatch("concatenation");
+        System.out.println(w.scoreGuess("ten"));
+        System.out.println(w.scoreGuess("nation"));
+        System.out.println(w.findBetterGuess("ten", "nation"));
+        System.out.println(w.scoreGuess("con"));
+        System.out.println(w.scoreGuess("cat"));
+        System.out.println(w.findBetterGuess("con", "cat"));
+    }
+}
