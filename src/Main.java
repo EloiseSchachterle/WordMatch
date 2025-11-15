8a7770d4e54347d56ffeb1db510ffebca74b5449
EloiseSchachterle <eloises3@nycstudents.net>
2025-11-14 22:53:59 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         WordMatch w = new WordMatch("concatenation");
         System.out.println(w.scoreGuess("ten"));
         System.out.println(w.scoreGuess("nation"));
@@ -9,5 +12,19 @@ public class Main {
         System.out.println(w.scoreGuess("con"));
         System.out.println(w.scoreGuess("cat"));
         System.out.println(w.findBetterGuess("con", "cat"));
+        System.out.println(read());
     }
+    public static int read() throws FileNotFoundException{
+        File a = new File("Guesses.txt");
+        Scanner b = new Scanner(a);
+        int guess1;
+        int guess2;
+        int num = 0;
+        while (b.hasNext()){
+            WordMatch c = new WordMatch(b.next());
+            num += c.scoreGuess(c.findBetterGuess(b.next(), b.next()));
+        }
+        return num;
+    }
+
 }
